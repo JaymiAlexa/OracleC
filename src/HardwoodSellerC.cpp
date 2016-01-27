cb52d805365342b13bd0f1f0258659254ef657a0
Jaymi Johnson <jaj14b@my.fsu.edu>
2016-01-27 14:56:39 -0500

    Added global variables
    Start program

--- src/HardwoodSellerC.cpp
+++ src/HardwoodSellerC.cpp
@@ -7,7 +7,18 @@
 //============================================================================
 
 #include <iostream>
+#include "WoodItem.cpp"
 using namespace std;
+
+Cherry = new WoodItem("Cherry", 2.5, 5.95);
+Maple = new WoodItem("Curly Maple", 1.5, 6.00);
+Mahogany = new WoodItem("Genuine Mahogany", 3, 9.60);
+Wenge = new WoodItem("Wenge", 5, 22.35);
+Oak = new WoodItem("White Oak", 2.3, 6.70);
+Sawdust = new WoodItem("Sawdust", 1, 1.5);
+
+
+
 
 int main() {
 	cout
@@ -20,7 +31,7 @@ int main() {
  * Method to read the input file
  */
 void readInputFile(string inputFilePath) {
-
+	string* input = new string[]
 }
 
 /*
